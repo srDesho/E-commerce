3fea946076a6eb6878af78500e064151ab205543
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-06-03 14:22:14 -0400

    Creating UserModel and AuthorizeModel entities

---
+++ src/main/java/com/cristianml/models/AuthorizeModel.java
@@ -0,0 +1,31 @@
+package com.cristianml.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "authorize")
+public class AuthorizeModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "id_user")
+    private UserModel user;
+
+}
