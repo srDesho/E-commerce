ff7b0107833996540c754932c8adf143a53502a9
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-06-16 21:51:12 -0400

    HomeController path changed

--- src/main/java/com/cristianml/controllers/HomeController.java
+++ src/main/java/com/cristianml/controllers/HomeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("/ecommerce")
+@RequestMapping("/ecommerce/customer")
 public class HomeController {
 
     @Autowired
