bd0e6184e31feb7d1eb9898c92bbb25eaa658124
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-07-04 16:51:20 -0400

    CartDAOImplTest test class completed

--- src/test/java/com/cristianml/persistence/impl/CartDAOImplTest.java
+++ src/test/java/com/cristianml/persistence/impl/CartDAOImplTest.java
@@ -1,4 +1,51 @@
 package com.cristianml.persistence.impl;
 
+import com.cristianml.dataProvider.CartDataProvider;
+import com.cristianml.dataProvider.UserDataProvider;
+import com.cristianml.models.CartModel;
+import com.cristianml.repository.CartRepository;
+import com.cristianml.security.model.UserModel;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class CartDAOImplTest {
+
+    @Mock
+    CartRepository cartRepository;
+
+    @InjectMocks
+    CartDAOImpl cartDAO;
+
+    // save method
+    @Test
+    public void testSave() {
+        CartModel cart = CartDataProvider.optionalCartMock().get();
+
+        when(this.cartRepository.save(any(CartModel.class))).thenReturn(cart);
+        CartModel result = this.cartDAO.save(cart);
+
+        assertNotNull(result);
+        assertEquals(1L, result.getId());
+    }
+
+    // findCartByUser method
+    @Test
+    public void testFindCartByUser() {
+        UserModel user = UserDataProvider.optionalUserMock().get();
+        CartModel cart = CartDataProvider.optionalCartMock().get();
+
+        when(this.cartRepository.findCartModelByUser(any(UserModel.class))).thenReturn(Optional.of(cart));
+        Optional<CartModel> result = this.cartDAO.findCartByUser(user);
+
+        assertEquals(user.getId(), result.get().getUser().getId());
+    }
 }
