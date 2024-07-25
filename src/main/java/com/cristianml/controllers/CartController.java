ce501310958f8a41b0201d84b7af92aaeb8b5d0f
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-07-25 17:11:11 -0400

    Delete CartItem

--- src/main/java/com/cristianml/controllers/CartController.java
+++ src/main/java/com/cristianml/controllers/CartController.java
@@ -1,9 +1,12 @@
 package com.cristianml.controllers;
 
+import com.cristianml.models.CartItemModel;
 import com.cristianml.models.CartModel;
 import com.cristianml.persistence.ICartDAO;
+import com.cristianml.persistence.ICartItemDAO;
 import com.cristianml.persistence.ICategoryDAO;
 import com.cristianml.persistence.impl.CartDAOImpl;
+import com.cristianml.service.ICartItemService;
 import com.cristianml.service.impl.CartServiceImpl;
 import com.cristianml.service.impl.OrderServiceImpl;
 import com.cristianml.service.impl.UserServiceImpl;
@@ -12,6 +15,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/ecommerce/customer/cart")
@@ -24,13 +30,15 @@ public class CartController {
     private final CartServiceImpl cartService;
     private final OrderServiceImpl orderService;
     private final ICategoryDAO categoryService;
+    private final ICartItemDAO iCartItemDAO;
 
     public CartController(UserServiceImpl userService, CartServiceImpl cartService, OrderServiceImpl orderService,
-                          ICategoryDAO categoryService) {
+                          ICategoryDAO categoryService, ICartItemDAO iCartItemDAO) {
         this.userService = userService;
         this.cartService = cartService;
         this.orderService = orderService;
         this.categoryService = categoryService;
+        this.iCartItemDAO = iCartItemDAO;
     }
 
 
@@ -79,6 +87,25 @@ public class CartController {
     @GetMapping("/success")
     public String success() {
         return "/success";
+    }
+
+    // Delete item
+    @GetMapping("/delete-item/{itemId}")
+    public String deleteItem(@PathVariable("itemId") Long itemId, Model model, RedirectAttributes flash) {
+       // Get the item
+        Optional<CartItemModel> cartItemDAO = this.iCartItemDAO.findCartItemModelById(itemId);
+
+        // Verify if exists
+        if (cartItemDAO.isEmpty()) {
+            flash.addFlashAttribute("clas", "danger");
+            flash.addFlashAttribute("message", "Item could not be removed try again later");
+        } else {
+            this.iCartItemDAO.deleteById(itemId);
+            flash.addFlashAttribute("clas", "success");
+            flash.addFlashAttribute("message", "Item deleted successfully");
+        }
+
+        return "redirect:/ecommerce/customer/cart/view";
     }
 
     // Generics
